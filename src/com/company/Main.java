268e44ff6989cc133a0dd1ccf20f05a12aae88d1
mustafa-ylmaz <98712045+mustafa-ylmaz@users.noreply.github.com>
2022-01-30 23:36:42 +0300

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,31 @@
 package com.company;
 
-import java.util.Locale;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+    int mat,muzik,fizik,kimya,turkce,tarih;
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Matematik notunuz:");
+        mat = scanner.nextInt();
+        System.out.println("muzik notunuz:");
+        muzik = scanner.nextInt();
+        System.out.println("fizik notunuz:");
+        fizik = scanner.nextInt();
+        System.out.println("kimya notunuz:");
+        kimya = scanner.nextInt();
+        System.out.println("turkce notunuz:");
+        turkce = scanner.nextInt();
+        System.out.println("tarih notunuz:");
+        tarih = scanner.nextInt();
 
+        int toplam = (muzik+mat+fizik+kimya+turkce+tarih);
+        double sonuc = toplam/6.0;
+        System.out.println("ortalamanız: "+sonuc);
+        System.out.println(sonuc>60?"Geçtiniz":"kaldınız");
     }
 }
 
